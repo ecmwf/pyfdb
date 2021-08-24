dd321546dffe9ac9d9f8288eacb4a09ca99095d2
majh <James.Hawkes@ecmwf.int>
2021-08-24 15:17:34 +0100

    Convert to a package for reliable distribution with includes (.h file)

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
     name='pyfdb',
     install_requires=['cffi'],
-    py_modules=['pyfdb'],
-    include_package_data = True
+    packages = find_packages(),
+    include_package_data = True,
+    description = 'Python interface to FDB',
+    url = 'https://git.ecmwf.int/projects/MARS/repos/pyfdb/browse',
+    author = 'ECMWF',
+    author_email = 'software.support@ecmwf.int',
 )
