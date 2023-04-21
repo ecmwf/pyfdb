0d7505836178dbfe4cfa810e20ac4991ef345bc2
Sandor Kertesz <Sandor.Kertesz@ecmwf.int>
2023-04-21 11:38:59 +0000

    Reformat with black 23.3.0

--- tests/test.py
+++ tests/test.py
@@ -16,72 +16,72 @@ fdb = pyfdb.FDB()
 
 ### Archive ###
 key = {
-    'domain': 'g',
-    'stream': 'oper',
-    'levtype': 'pl',
-    'levelist': '300',
-    'date': '20191110',
-    'time': '0000',
-    'step': '0',
-    'param': '138',
-    'class': 'rd',
-    'type': 'an',
-    'expver': 'xxxx'
+    "domain": "g",
+    "stream": "oper",
+    "levtype": "pl",
+    "levelist": "300",
+    "date": "20191110",
+    "time": "0000",
+    "step": "0",
+    "param": "138",
+    "class": "rd",
+    "type": "an",
+    "expver": "xxxx",
 }
 
-filename = 'x138-300.grib'
+filename = "x138-300.grib"
 fdb.archive(open(filename, "rb").read(), key)
 
-key['levelist'] = '400'
-filename = 'x138-400.grib'
+key["levelist"] = "400"
+filename = "x138-400.grib"
 pyfdb.archive(open(filename, "rb").read())
 
-key['expver'] = 'xxxy'
-filename = 'y138-400.grib'
+key["expver"] = "xxxy"
+filename = "y138-400.grib"
 fdb.archive(open(filename, "rb").read())
 fdb.flush()
 
 ### List ###
 request = {
-    'class': 'rd',
-    'expver': 'xxxx',
-    'stream': 'oper',
-    'date': '20191110',
-    'time': '0000',
-    'domain': 'g',
-    'type': 'an',
-    'levtype': 'pl',
-    'step': 0,
-    'levelist': [300, '500'],
-    'param': ['138', 155, 't']
+    "class": "rd",
+    "expver": "xxxx",
+    "stream": "oper",
+    "date": "20191110",
+    "time": "0000",
+    "domain": "g",
+    "type": "an",
+    "levtype": "pl",
+    "step": 0,
+    "levelist": [300, "500"],
+    "param": ["138", 155, "t"],
 }
-print('direct function, request as dictionary:', request)
+print("direct function, request as dictionary:", request)
 for el in pyfdb.list(request, True):
-    assert(el['path'])
-    assert(el['path'].find('rd:xxxx:oper:20191110:0000:g/an:pl.') != -1)
-    assert(not 'keys' in el)
+    assert el["path"]
+    assert el["path"].find("rd:xxxx:oper:20191110:0000:g/an:pl.") != -1
+    assert not "keys" in el
 
-request['levelist'] = ['100', '200', '300', '400', '500', '700', '850', '1000']
-request['param'] = '138'
-print('')
-print('direct function, updated dictionary:', request)
+request["levelist"] = ["100", "200", "300", "400", "500", "700", "850", "1000"]
+request["param"] = "138"
+print("")
+print("direct function, updated dictionary:", request)
 it = iter(pyfdb.list(request, True, True))
 
 el = next(it)
-assert(el['path'])
-assert(el['path'].find('rd:xxxx:oper:20191110:0000:g/an:pl.') != -1)
-assert(el['keys'])
-keys = el['keys']
-assert(keys['class'] == 'rd')
-assert(keys['levelist'] == '300')
+assert el["path"]
+assert el["path"].find("rd:xxxx:oper:20191110:0000:g/an:pl.") != -1
+assert el["keys"]
+keys = el["keys"]
+assert keys["class"] == "rd"
+assert keys["levelist"] == "300"
 
 el = next(it)
-assert(el['path'])
-assert(el['path'].find('rd:xxxx:oper:20191110:0000:g/an:pl.') != -1)
-assert(el['keys'])
-keys = el['keys']
-assert(keys['class'] == 'rd')
-assert(keys['levelist'] == '400')
+assert el["path"]
+assert el["path"].find("rd:xxxx:oper:20191110:0000:g/an:pl.") != -1
+assert el["keys"]
+keys = el["keys"]
+assert keys["class"] == "rd"
+assert keys["levelist"] == "400"
 
 try:
     el = next(it)
@@ -91,51 +91,50 @@ except StopIteration:
 
 
 # as an alternative, create a FDB instance and start queries from there
-request['levelist'] = ['400', '500', '700', '850', '1000']
-print('')
-print('fdb object, request as dictionary:', request)
+request["levelist"] = ["400", "500", "700", "850", "1000"]
+print("")
+print("fdb object, request as dictionary:", request)
 for el in fdb.list(request, True, True):
-    assert(el['path'])
-    assert(el['path'].find('rd:xxxx:oper:20191110:0000:g/an:pl.') != -1)
-    assert(el['keys'])
-    keys = el['keys']
-    assert(keys['class'] == 'rd')
-    assert(keys['levelist'] == '400')
-    
+    assert el["path"]
+    assert el["path"].find("rd:xxxx:oper:20191110:0000:g/an:pl.") != -1
+    assert el["keys"]
+    keys = el["keys"]
+    assert keys["class"] == "rd"
+    assert keys["levelist"] == "400"
+
 
 ### Retrieve ###
 request = {
-    'domain': 'g',
-    'stream': 'oper',
-    'levtype': 'pl',
-    'step': '0',
-    'expver': 'xxxx',
-    'date': '20191110',
-    'class': 'rd',
-    'levelist': '300',
-    'param': '138',
-    'time': '0000',
-    'type': 'an'
+    "domain": "g",
+    "stream": "oper",
+    "levtype": "pl",
+    "step": "0",
+    "expver": "xxxx",
+    "date": "20191110",
+    "class": "rd",
+    "levelist": "300",
+    "param": "138",
+    "time": "0000",
+    "type": "an",
 }
 
-filename = 'x138-300bis.grib'
-print('')
-print('save to file ', filename)
-with open(filename, 'wb') as o, fdb.retrieve(request) as i:
+filename = "x138-300bis.grib"
+print("")
+print("save to file ", filename)
+with open(filename, "wb") as o, fdb.retrieve(request) as i:
     shutil.copyfileobj(i, o)
 
-request['levelist'] = '400'
-filename = 'x138-400bis.grib'
-print('save to file ', filename)
-with open(filename, 'wb') as o, fdb.retrieve(request) as i:
+request["levelist"] = "400"
+filename = "x138-400bis.grib"
+print("save to file ", filename)
+with open(filename, "wb") as o, fdb.retrieve(request) as i:
     shutil.copyfileobj(i, o)
 
-request['expver'] = 'xxxy'
-filename = 'y138-400bis.grib'
-print('save to file ', filename)
-with open(filename, 'wb') as o, pyfdb.retrieve(request) as i:
+request["expver"] = "xxxy"
+filename = "y138-400bis.grib"
+print("save to file ", filename)
+with open(filename, "wb") as o, pyfdb.retrieve(request) as i:
     shutil.copyfileobj(i, o)
-
 
 
 # request = {
@@ -150,42 +149,41 @@ with open(filename, 'wb') as o, pyfdb.retrieve(request) as i:
 #     'step': 0,
 #     'param': 151
 # }
-print('')
-print('FDB retrieve')
-print('direct function, retrieve from request:', request)
+print("")
+print("FDB retrieve")
+print("direct function, retrieve from request:", request)
 datareader = pyfdb.retrieve(request)
 
-print('')
-print('reading a small chunk')
+print("")
+print("reading a small chunk")
 chunk = datareader.read(10)
 print(chunk)
-print('tell()', datareader.tell())
+print("tell()", datareader.tell())
 
-print('go back (partially) - seek(2)')
+print("go back (partially) - seek(2)")
 datareader.seek(2)
-print('tell()', datareader.tell())
+print("tell()", datareader.tell())
 
-print('reading a larger chunk')
+print("reading a larger chunk")
 chunk = datareader.read(40)
 print(chunk)
 
-print('go back - seek(0)')
+print("go back - seek(0)")
 datareader.seek(0)
 
-print('')
-print('decode GRIB')
+print("")
+print("decode GRIB")
 from eccodes import StreamReader
 
 reader = StreamReader(datareader)
 grib = next(reader)
 grib.dump()
 
-request['levelist'] = [300, '400']
-request['expver'] = 'xxxx'
-filename = 'foo.grib'
+request["levelist"] = [300, "400"]
+request["expver"] = "xxxx"
+filename = "foo.grib"
 
-print('')
-print('save to file ', filename)
-with open(filename, 'wb') as o, fdb.retrieve(request) as i:
+print("")
+print("save to file ", filename)
+with open(filename, "wb") as o, fdb.retrieve(request) as i:
     shutil.copyfileobj(i, o)
-
