7f4fd38b302f2e6437b391fe6f7f855690872e72
Tom <thomas.hodson@ecmwf.int>
2024-10-10 09:55:34 +0100

    Add fdb_home argument

--- tests/test_passing_config_directly.py
+++ tests/test_passing_config_directly.py
@@ -1,7 +1,61 @@
+import shutil
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
+from pytest import raises
+
 import pyfdb
+
+
+def test_conflicting_arguments():
+    "You can't pass both fdb_home and config because the former overrides the latter"
+    assert raises(ValueError, pyfdb.FDB, fdb_home="/tmp", config={})
+
+
+def test_fdb_home():
+    with TemporaryDirectory() as tmp_home:
+        tests_dir = Path(__file__).parent
+        schema_path = tests_dir / "default_fdb_schema"
+
+        home = Path(tmp_home)
+        etc = home / "etc" / "fdb"
+        root_path = home / "root"
+        root_path.mkdir(parents=True)
+        etc.mkdir(parents=True)
+
+        shutil.copy(schema_path, etc / "schema")
+
+        with open(etc / "config.yaml", "w") as f:
+            f.write(
+                f"""
+---
+type: local
+engine: toc
+schema: {etc / "schema"}
+spaces:
+- handler: Default
+  roots:
+    - path: "{root_path}"
+      writable: true
+      visit: true
+"""
+            )
+
+        with open(etc / "config.yaml", "r") as f:
+            print(f.read())
+
+        fdb = pyfdb.FDB(fdb_home=home)
+        data = open(tests_dir / "x138-300.grib", "rb").read()
+        fdb.archive(data)
+        fdb.flush()
+
+        list_output = list(fdb.list(keys=True))
+        assert len(list_output) == 1
+
+        # Check that the archive path is in the tmp directory
+        # On OSX tmp file paths look like /private/var/folders/.../T/tmp.../x138-300.grib
+        # While the tmp directory looks like /var/folders/.../T/tmp.../ hence why this check is not "startwith"
+        assert tmp_home in list_output[0]["path"]
 
 
 def test_direct_config():
